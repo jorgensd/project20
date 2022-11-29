0b16e6b1667cde510a879b4ea74287364d10778c
Jørgen S. Dokken <dokken@simula.no>
2022-11-29 09:47:21 +0100

    Try using julia on CI

---
+++ helloworld.jl
@@ -0,0 +1,14 @@
+import Pkg; Pkg.add("Plots")
+using Plots
+ENV["GKSwstype"]="nul"
+
+function f(x)
+    sin.(x).+ 2.
+end
+
+
+x = range(0,stop=2*pi,length=10)
+plot(x, f(x), fmt= :png)
+rm("plots", recursive=true)
+mkdir("plots")
+savefig("plots/f.png")
